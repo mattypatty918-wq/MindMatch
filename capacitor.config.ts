821e1915c2a547ac0e666e98c9de93ff06092ad8
Mathew Simpson <mattypattysapps@zo.computer>
2026-05-20 00:46:40 +0000

    rename app and finalize ios build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.worddrop.game',
-  appName: 'Word Drop',
+  appId: 'com.mattypattysapps.flipmatch',
+  appName: 'Flip Match',
   webDir: 'dist',
   server: { androidScheme: 'https' },
   plugins: {
